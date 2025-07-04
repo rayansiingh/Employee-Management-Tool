a34b63348a499fb5266f3050ea9f7ce4e940cb57
Rayan Singh <rayansiingh@gmail.com>
2025-07-04 13:42:04 +0530

    Basic login page structure done

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
-const Login = () => {
+function Login() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="border-2 border-red-600">
-        <form className="flex-col flex">
-          <input type="email" placeholder="Enter email" />
-          <input type="password" placeholder="Enter password" />
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="bg-[#7e7f83] rounded-lg py-10 px-15 border-2">
+        <form className="flex flex-col items-center justify-center gap-4">
+          <input
+            required
+            className="bg-[#FFFBFF] rounded-full py-3 px-5 text-xl outline-none placeholder:text-zinc-700 mt-8 border-1"
+            type="email"
+            placeholder="Email address"
+          />
+          <input
+            required
+            className="bg-[#FFFBFF] rounded-full py-3 px-5 text-xl outline-none placeholder:text-zinc-700 border-1"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-[#90FFDC] border-1 border-black rounded-full py-2 px-5 text-lg mt-8 font-base">
+            Log In
+          </button>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
