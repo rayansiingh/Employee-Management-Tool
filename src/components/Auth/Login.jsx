3c18fe079edf8ae6236e4ec9b9a96a8769cc1851
Rayan Singh <rayansiingh@gmail.com>
2025-07-06 17:12:12 +0530

    applied react-hook-form

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,25 +1,36 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const { register, handleSubmit, reset } = useForm();
+  const handleFormSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
   return (
     <div className="flex items-center justify-center h-screen w-screen">
       <div className="bg-[#7e7f83] rounded-lg py-10 px-15 border-2">
-        <form className="flex flex-col items-center justify-center gap-4">
+        <form
+          className="flex flex-col items-center justify-center gap-4"
+          onSubmit={handleSubmit(handleFormSubmit)}
+        >
           <input
-            required
+            {...register("email", { required: true })}
             className="bg-[#FFFBFF] rounded-full py-3 px-5 text-xl outline-none placeholder:text-zinc-700 mt-8 border-1"
             type="email"
             placeholder="Email address"
           />
           <input
-            required
+            {...register("password", { required: true })}
             className="bg-[#FFFBFF] rounded-full py-3 px-5 text-xl outline-none placeholder:text-zinc-700 border-1"
             type="password"
             placeholder="Password"
           />
-          <button className="bg-[#90FFDC] border-1 border-black rounded-full py-2 px-5 text-lg mt-8 font-base">
-            Log In
-          </button>
+          <input
+            className="bg-[#90FFDC] border-1 border-black rounded-full py-2 px-5 text-lg mt-8 font-base"
+            type="submit"
+            value="Log In"
+          />
         </form>
       </div>
     </div>
